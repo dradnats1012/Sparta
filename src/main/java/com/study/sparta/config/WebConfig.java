6a6a4c015dc7e0b90b12905417a8486f3efa6eb8
dradnats1012 <herjebi1012@gmail.com>
2025-07-25 21:14:46 +0900

    fix: CORS 수정

--- src/main/java/com/study/sparta/config/WebConfig.java
+++ src/main/java/com/study/sparta/config/WebConfig.java
@@ -9,7 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins(
+                "http://localhost:3000",
+                "http://192.168.45.223:3000"
+            )
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("*")
             .allowCredentials(true);
