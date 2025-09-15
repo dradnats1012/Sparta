8a60ec26c999b431dd617f94d1f8c4fbf4aa843a
dradnats1012 <herjebi1012@gmail.com>
2025-09-15 20:34:22 +0900

    refactor: 스웨거 mix 설정

--- src/main/java/com/study/sparta/config/SwaggerConfig.java
+++ src/main/java/com/study/sparta/config/SwaggerConfig.java
@@ -3,10 +3,19 @@ package com.study.sparta.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
+@OpenAPIDefinition(
+    servers = {
+        @Server(url = "https://sparta-api.herjebi.com", description = "spparta https"),
+        @Server(url = "http://sparta-api.herjebi.com", description = "sparta http"),
+        @Server(url = "http://localhost:8081", description = "sparta local")
+    }
+)
 @Configuration
 public class SwaggerConfig {
     @Bean
