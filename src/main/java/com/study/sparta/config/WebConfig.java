981b81e3770ba7efd2fc9c10bc9c7ab0c076b752
dradnats1012 <herjebi1012@gmail.com>
2025-10-15 13:33:38 +0900

    feat: Server-Timing 헤더 추가

--- src/main/java/com/study/sparta/config/WebConfig.java
+++ src/main/java/com/study/sparta/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.study.sparta.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.RequiredArgsConstructor;
@@ -10,11 +11,18 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
+    private final ServerTimingInterceptor serverTimingInterceptor;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("*");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(serverTimingInterceptor).addPathPatterns("/api/**");
     }
 }
